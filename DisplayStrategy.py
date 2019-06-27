75ff9ab9ff9d8ca07045915a0547146d7278aecc
Frederk <Frederik1904@gmail.com>
2019-06-27 19:03:42 +0200

    Initial commit

---
+++ DisplayStrategy.py
@@ -0,0 +1,42 @@
+from abc import ABCMeta, abstractclassmethod
+
+
+class DisplayInterface:
+    ___metaclass___ = ABCMeta
+
+    @classmethod
+    def version(cls):
+        return "1.0"
+
+    @classmethod
+    def show_msg(cls, msg):
+        raise NotImplementedError
+
+
+class ConsoleDisplay(DisplayInterface):
+    @classmethod
+    def show_msg(cls, msg):
+        print(msg)
+
+
+class LCDDisplay(DisplayInterface):
+    
+
+#
+# class MyClient(object):
+#     def __init__(self, server):
+#         if not isinstance(server, DisplayInterface):
+#             raise Exception("Bad interface")
+#
+#         if not DisplayInterface.version() == "1.0":
+#             raise Exception("Bad version")
+#
+#         self.__server = server
+#
+#     def client_show(cls):
+#         cls.__server.show()
+#
+#
+# x = MyClient(MyServer())
+#
+# x.client_show()
---
+++ Main.py
@@ -0,0 +1,7 @@
+import DisplayStrategy as ds
+
+# Display adapter change this to the specific strategy to use.
+x = ds.ConsoleDisplay()
+
+
+x.show_msg("hello world!!!")
