72cff314263dfadc9376fa26ed027b5a31cbe621
Frederk <Frederik1904@gmail.com>
2019-06-29 15:30:39 +0200

    whoops

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import DisplayStrategy as ds
+import WeightStrategy as ws
 import time
 
 # Display adapter change this to the specific strategy to use.
-x = ds.ConsoleDisplay()
+display = ds.ConsoleDisplay()
+weight = ws.FakeWeight(display)
 
-for i in range(100):
-    x.show_msg(f"hello: {i}")
-    time.sleep(1)
+weight.initialize_weights()
