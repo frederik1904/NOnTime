e71d828467873695282f75008393f680d048c635
Frederik <Frederik1904@gmail.com>
2019-07-15 15:34:49 +0200

    ?

--- DisplayStrategy.py
+++ DisplayStrategy.py
@@ -43,4 +43,67 @@ class LCDDisplay(DisplayInterface):
     @classmethod
     def show_msg(cls, msg):
         cls.lcd.clear()
-        lcd_text(msg,LCD_LINE_
+        lcd_text(msg,LCD_LINE_1)
+
+    # Initialize and clear display
+    def lcd_init():
+        lcd_write(0x33,LCD_CMD) # Initialize
+        lcd_write(0x32,LCD_CMD) # Set to 4-bit mode
+        lcd_write(0x06,LCD_CMD) # Cursor move direction
+        lcd_write(0x0C,LCD_CMD) # Turn cursor off
+        lcd_write(0x28,LCD_CMD) # 2 line display
+        lcd_write(0x01,LCD_CMD) # Clear display
+        time.sleep(0.0005) # Delay to allow commands to process
+
+    def lcd_write(bits, mode):
+        # High bits
+        GPIO.output(LCD_RS, mode) # RS
+
+        GPIO.output(LCD_D4, False)
+        GPIO.output(LCD_D5, False)
+        GPIO.output(LCD_D6, False)
+        GPIO.output(LCD_D7, False)
+        if bits&0x10==0x10:
+            GPIO.output(LCD_D4, True)
+        if bits&0x20==0x20:
+            GPIO.output(LCD_D5, True)
+        if bits&0x40==0x40:
+            GPIO.output(LCD_D6, True)
+        if bits&0x80==0x80:
+            GPIO.output(LCD_D7, True)
+
+        # Toggle 'Enable' pin
+        lcd_toggle_enable()
+
+        # Low bits
+        GPIO.output(LCD_D4, False)
+        GPIO.output(LCD_D5, False)
+        GPIO.output(LCD_D6, False)
+        GPIO.output(LCD_D7, False)
+        if bits&0x01==0x01:
+            GPIO.output(LCD_D4, True)
+        if bits&0x02==0x02:
+            GPIO.output(LCD_D5, True)
+        if bits&0x04==0x04:
+            GPIO.output(LCD_D6, True)
+        if bits&0x08==0x08:
+            GPIO.output(LCD_D7, True)
+
+        # Toggle 'Enable' pin
+        lcd_toggle_enable()
+
+    def lcd_toggle_enable():
+        time.sleep(0.0005)
+        GPIO.output(LCD_E, True)
+        time.sleep(0.0005)
+        GPIO.output(LCD_E, False)
+        time.sleep(0.0005)
+
+    def lcd_text(message,line):
+        # Send text to display
+        message = message.ljust(LCD_CHARS," ")
+
+        lcd_write(line, LCD_CMD)
+
+        for i in range(LCD_CHARS):
+            lcd_write(ord(message[i]),LCD_CHR)
